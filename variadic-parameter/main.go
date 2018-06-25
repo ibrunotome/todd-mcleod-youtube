ccadbd080386c6bd99baae14bcc9080249344d7b
Bruno Tomé <ibrunotome@gmail.com>
2018-06-24 21:15:24 -0300

    Variadic parameter

---
+++ variadic-parameter/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := sum(2, 3, 4, 5, 6, 7, 8, 9)
+
+	fmt.Println("The total is", x)
+}
+
+func sum(x ...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index position", i, "we are now adding", v, "to the total wich is now", sum)
+	}
+
+	return sum
+}
