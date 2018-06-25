b95089242d30e5fcdcb0728d6f4bd9c0ce3800c9
Bruno Tomé <ibrunotome@gmail.com>
2018-06-25 08:39:05 -0300

    Understanding interfaces

--- methods/main.go
+++ methods/main.go
@@ -8,13 +8,33 @@ type person struct {
 	first string
 	last  string
 }
+
 type secretAgent struct {
 	person
 	ltk bool
 }
 
 func (s secretAgent) speak() {
-	fmt.Println("I am", s.first, s.last)
+	fmt.Println("I am", s.first, s.last, " - the secretAgent speak")
+}
+
+func (p person) speak() {
+	fmt.Println("I am", p.first, p.last, " - the person speak")
+}
+
+func bar(h human) {
+	switch h.(type) {
+	case person:
+		fmt.Println("I was passed into bar", h.(person).first)
+	case secretAgent:
+		fmt.Println("I was passed into bar", h.(secretAgent).first)
+	}
+
+	fmt.Println("I was passed into bar", h)
+}
+
+type human interface {
+	speak()
 }
 
 func main() {
@@ -34,7 +54,18 @@ func main() {
 		ltk: true,
 	}
 
+	p1 := person{
+		first: "Dr.",
+		last:  "Yes",
+	}
+
 	fmt.Println(sa1)
 	sa1.speak()
 	sa2.speak()
+
+	fmt.Println(p1)
+
+	bar(sa1)
+	bar(sa2)
+	bar(p1)
 }
